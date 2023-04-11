d6ee46879d98566e052da0cddb0ab6fb9324ed41
maxabzh <maxabzh@gmail.com>
2023-04-11 15:35:56 +0600

    create: rough draft of the react app

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import './index.scss';
+
+const ReactApp = () => <>React App</>;
+
+createRoot(document.getElementById('reactApp'))
+.render(<React.StrictMode><ReactApp /></React.StrictMode>);
